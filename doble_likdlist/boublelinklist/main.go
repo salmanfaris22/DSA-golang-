9652e0805687f38499e5ddaadedcbfa5b3ec50d6
salmanfaris22 <salman567633800@gmail.com>
2024-10-05 11:57:52 +0530

    dsa

--- doble_likdlist/boublelinklist/main.go
+++ doble_likdlist/boublelinklist/main.go
@@ -44,7 +44,7 @@ func (list *linkedlist) display() {
 func (list *linkedlist) reversedisplay() {
 	temp := list.tail
 	if list.head == nil {
-		fmt.Println("List is empty")
+		fmt.Println("\n List is empty")
 		return
 	}
 	fmt.Println("\nrevers Order")
@@ -72,14 +72,66 @@ func (list *linkedlist) addBeggining(num int) {
 	}
 }
 
+func (list *linkedlist) delitlast() {
+	if list.tail == nil {
+		fmt.Println("\n pleas enter Node")
+		return
+	}
+	if list.tail.prev == nil {
+		list.head = nil
+		list.tail = nil
+		return
+	}
+	list.tail.prev.next = nil
+	list.tail = list.tail.prev
+}
+func (list *linkedlist) deletfirst() {
+	if list.head == nil {
+		fmt.Println("\n pleas enter Node")
+		return
+	}
+	if list.head.next == nil {
+		list.head = nil
+		list.tail = nil
+		return
+	}
+	list.head = list.head.next
+	list.head.prev = nil
+}
+
+func (list *linkedlist) feseficdeilit(num int) {
+	temp := list.head
+	for temp.value != num {
+		temp = temp.next
+		if temp == nil {
+			fmt.Println("\n number cant find")
+			return
+		}
+	}
+	if temp.prev != nil {
+		temp.prev.next = temp.next
+	} else {
+		list.head = temp.next
+	}
+	if temp.next != nil {
+		temp.next.prev = temp.prev
+	} else {
+		list.tail = temp.prev
+	}
+
+}
 func main() {
 	list := &linkedlist{}
 	list.add(3)
 	list.add(4)
 	list.add(5)
 	list.add(6)
-	list.addBeggining(2)
-	list.addBeggining(1)
 	list.display()
+	list.feseficdeilit(3)
+
+	// list.deletfirst()
+	// list.deletfirst()
 	list.reversedisplay()
+	list.display()
+	// list.reversedisplay()
 }
